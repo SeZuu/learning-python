fa829b8bbb97ff7cb89fcb09a4c3ca6757f6605e
sezu <dukemlet@gmail.com>
2023-10-07 17:07:11 -0500

    new file -operators

---
+++ operators.py
@@ -0,0 +1,17 @@
+# Operators in python
+
+a = 5
+b = 3
+
+# Arithmetic operations
+addition = a + b      # 8
+diff = a - b          # 2
+product = a * b       # 16
+division = a / b      # 1.6
+floor_division = a//b # 1 -> se aproxima al entero mas cercano
+mod = a % b           # 2
+exponential = a ** b  # 125 (a^b)
+
+# + sirve para concatenar cadenas de texto
+print("this is a diff: " + str(diff))
+
