634aceba4f50c688b78f1b6a845f0c11b08ce54c
sezu <dukemlet@gmail.com>
2023-10-20 22:00:43 -0500

    other solution in the file

--- exercises/numbers_to_list.py
+++ exercises/numbers_to_list.py
@@ -4,4 +4,8 @@ tupla = tuple(lista)
 print(lista)
 print(tupla)
 
-# split() function elmina el valor que le pasemos de la cadena de texto y retorna una lista sin estos elementos
+# split() function elmina el valor que le pasemos de la cadena de texto y retorna una lista sin estos elemento
+
+"""filename = input("filename: ")
+f_exten = filename.split(".")
+print("the file extension is:", f_exten[-1])"""
