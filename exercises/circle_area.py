1c8e91b9719bd2ebb793bcdde1e99852be4ded5d
sezu <dukemlet@gmail.com>
2023-10-20 21:54:46 -0500

    just exercises

---
+++ exercises/circle_area.py
@@ -0,0 +1,8 @@
+from math import pi
+
+radius = float(input("radius: "))
+print("The area of the circle is: ", pi * radius**2)
+
+"""name = input("name: ")
+surname = input("surname: ")
+print(f"Hello {surname} {name}")"""
---
+++ exercises/numbers_to_list.py
@@ -0,0 +1,7 @@
+ls = input("data separated with commas: ")
+lista = ls.split(",")
+tupla = tuple(lista)
+print(lista)
+print(tupla)
+
+# split() function elmina el valor que le pasemos de la cadena de texto y retorna una lista sin estos elementos
